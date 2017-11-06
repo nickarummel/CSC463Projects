10a6b9d000d401143cd0c5d099fb9232f96f755f
Nick Rummel <nr0759@cs.ship.edu>
2017-11-06 11:50:42 -0500

    Assignment 6 - Initial Commit Java code to create a genetic algorithm tree.

---
+++ assignment6/src/assignment6/Runner.java
@@ -0,0 +1,23 @@
+package assignment6;
+
+/**
+ * Main class to generate the tree and code for
+ * the genetic algorithm in Interactive C.
+ * 
+ * @author Nick Rummel
+ * @author Alex Medellin
+ *
+ */
+public class Runner
+{
+
+	/*
+	 * Main method
+	 */
+	public static void main(String[] args)
+	{
+		
+
+	}
+
+}
