d62ad1007811ed85b57140cfc5506c6453d9331b
Nick Rummel <nr0759@cs.ship.edu>
2017-11-09 18:00:30 -0500

    Assignment 6 - Part 2 Completed
    Code is updated with comments and now works to replace a node of the
    tree with a new node (and possibly new sub tree).

--- assignment6/src/assignment6/RebuiltTree.java
+++ assignment6/src/assignment6/RebuiltTree.java
@@ -2,15 +2,20 @@ package assignment6;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Enumeration;
-
 import javax.swing.tree.DefaultMutableTreeNode;
 
+/**
+ * A class dedicated to rebuilding a
+ * tree stored in a text file.
+ * 
+ * @author Nick Rummel
+ * @author Alex Medellin
+ *
+ */
 public class RebuiltTree
 {
 	final private String INDENT = "     ";
@@ -18,6 +23,12 @@ public class RebuiltTree
 	private DefaultMutableTreeNode tree;
 	private ArrayList<String> text;
 	private int[] levels;
+	/**
+	 * Constructor of class.
+	 * Opens text file and rebuilds
+	 * the tree.
+	 * @param path the path to the file
+	 */
 	public RebuiltTree(String path)
 	{
 		text = new ArrayList<String>();
@@ -27,6 +38,12 @@ public class RebuiltTree
 		generateTree();
 	}
 
+	/**
+	 * Opens a file at a particular path
+	 * and reads in the text, storing each
+	 * line as a String in an ArrayList
+	 * @param path
+	 */
 	private void openFile(String path)
 	{
 		File tf = new File(path);
@@ -37,7 +54,6 @@ public class RebuiltTree
 		}
 		catch (FileNotFoundException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -50,16 +66,18 @@ public class RebuiltTree
 			}
 		} catch (IOException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Creates the tree and stores it
+	 * as an instance variable
+	 */
 	private void generateTree()
 	{
 		ArrayList<Integer> sameLevels = null;
 		calculateLevelCounts();
-		//int size = text.size();
 		tree.setUserObject(text.get(0));
 		DefaultMutableTreeNode temp = tree;
 		for(int i = 1; i <= MAX_LEVEL; i++)
@@ -83,6 +101,11 @@ public class RebuiltTree
 		}
 	}
 	
+	/**
+	 * Loops through the String ArrayList
+	 * to determine what level of the tree
+	 * each line is located on.
+	 */
 	private void calculateLevelCounts()
 	{
 		int size = text.size();
@@ -107,6 +130,12 @@ public class RebuiltTree
 		}
 	}
 	
+	/**
+	 * Concatenates whitespace to determine
+	 * what level each node is on in the tree
+	 * @param count the number of indents to add
+	 * @return the whitespace as a String
+	 */
 	private String createIndent(int count)
 	{
 		String s = "";
@@ -115,6 +144,11 @@ public class RebuiltTree
 		return s;
 	}
 	
+	/**
+	 * Determines if any nodes are on the same level
+	 * @param level the level to be searched for
+	 * @return an ArrayList of nodes on the same level
+	 */
 	private ArrayList<Integer> checkForSameLevel(int level)
 	{
 		ArrayList<Integer> sameLevel = new ArrayList<Integer>();
@@ -128,11 +162,22 @@ public class RebuiltTree
 		return sameLevel;
 	}
 	
+	/**
+	 * Retrieves tree instance variable
+	 * @return the tree
+	 */
 	public DefaultMutableTreeNode getTree()
 	{
 		return tree;
 	}
 	
+	/**
+	 * Determines if the child node is a leaf (meaning that
+	 * this command does not have any children because it is a
+	 * turn, backup, or forward command).
+	 * @param node the parent of the node to be checked
+	 * @return True if child is "dead end", else false
+	 */
 	private boolean checkIfDeadEndComponent(DefaultMutableTreeNode node)
 	{
 		if(((String)node.getNextNode().getUserObject()).contains("Forward"))
--- assignment6/src/assignment6/Runner.java
+++ assignment6/src/assignment6/Runner.java
@@ -2,7 +2,6 @@ package assignment6;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.Enumeration;
 
 import javax.swing.tree.*;
@@ -17,6 +16,10 @@ import javax.swing.tree.*;
 public class Runner
 {
 	private final static int THRESHOLD = 235;
+	/**
+	 * Main method for project
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
 		DefaultMutableTreeNode root = chooseComponent(generateRandomSensor(),generateRandomComponent(),THRESHOLD);
@@ -28,12 +31,24 @@ public class Runner
 		System.out.println("");
 		
 		RebuiltTree rt = new RebuiltTree("output_tree.txt");
-		printTree(rt.getTree());
+		DefaultMutableTreeNode newTree = rt.getTree();
+		printTree(newTree);
+		
+		System.out.println("");
+		
+		newTree = changeOneNode(newTree, selectRandomNode(newTree));
+		
+		printTree(newTree);
 	}
 
+	/**
+	 * Converts the entire tree into a formatted String
+	 * @param root the start of the tree
+	 * @return the generated String
+	 */
 	public static String toString(DefaultMutableTreeNode root)
 	{
-		Enumeration en = root.preorderEnumeration();
+		Enumeration<?> en = root.preorderEnumeration();
 		String buf = "";
 		while(en.hasMoreElements())
 		{
@@ -50,11 +65,20 @@ public class Runner
 		return buf;
 	}
 	
+	/**
+	 * Prints out the tree to the user
+	 * @param root the start of the tree
+	 */
 	public static void printTree(DefaultMutableTreeNode root)
 	{
 			System.out.print(toString(root));
 	}
 	
+	/**
+	 * The tree is written to a designated text file in the project
+	 * using the ISO-8859-1 (Latin-1) character set.
+	 * @param root the start of the tree
+	 */
 	public static void exportTree(DefaultMutableTreeNode root)
 	{
 		String buf = toString(root);
@@ -65,11 +89,9 @@ public class Runner
 			writer.close();
 		} catch (FileNotFoundException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (UnsupportedEncodingException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -103,6 +125,7 @@ public class Runner
 	 * @param sensor The sensor number to be used
 	 * @param component The component to be used
 	 * @param threshold The threshold to be used
+	 * @return the root of tree
 	 */
 	public static DefaultMutableTreeNode chooseComponent(int sensor, int component, int threshold)
 	{
@@ -193,6 +216,69 @@ public class Runner
 		}
 		return node;
 		
+	}
+	
+	/**
+	 * Counts the number of nodes in the tree and 
+	 * randomly selects one of the nodes from that
+	 * count.
+	 * @param tree the root of the tree
+	 * @return the node number
+	 */
+	public static int selectRandomNode(DefaultMutableTreeNode tree)
+	{
+		Enumeration en = tree.preorderEnumeration();
+		int count = 0;
+		while(en.hasMoreElements())
+		{
+			en.nextElement();
+			count++;
+		}
+		return (int) Math.floor(((Math.random()*count)));
+	}
+	
+	/**
+	 * Part 2 of Assignment 6 - Replaces the node at the
+	 * designated location, removes children of node,
+	 * then removes the node from the parent. The node is 
+	 * then replaced, possibly generating a new sub-tree
+	 * off that node.
+	 * @param tree
+	 * @param nodeNum Node index
+	 * @return the root of the new tree
+	 */
+	public static DefaultMutableTreeNode changeOneNode(DefaultMutableTreeNode tree, int nodeNum)
+	{
+		DefaultMutableTreeNode temp = tree;
+		for(int i = 0; i < nodeNum; i++)
+			temp = temp.getNextNode();
+		temp.removeAllChildren();
+		if(temp.getSiblingCount() > 0 && !(temp.isRoot()))
+		{
+			DefaultMutableTreeNode test = (DefaultMutableTreeNode) temp.getParent().getChildAt(0);
+			if(test.equals(temp))
+			{
+				temp = (DefaultMutableTreeNode) temp.getParent();
+				temp.remove(0);
+			}
+			else
+			{
+				temp = (DefaultMutableTreeNode) temp.getParent();
+				temp.remove(1);
+			}
+			temp.add(chooseComponent(generateRandomSensor(), generateRandomComponent(), THRESHOLD));
+		}
+		else if(temp.getSiblingCount() == 0 && !(temp.isRoot()))
+		{
+			temp = (DefaultMutableTreeNode) temp.getParent();
+			temp.removeAllChildren();
+			temp.add(chooseComponent(generateRandomSensor(), generateRandomComponent(), THRESHOLD));
+		}
+		else 
+		{
+			temp = chooseComponent(generateRandomSensor(),generateRandomComponent(),THRESHOLD);
+		}
+		return (DefaultMutableTreeNode) temp.getRoot();
 	}
 
 }
